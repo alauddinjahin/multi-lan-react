194dfadad8fcb0c298321a9463101a4d000d9d28
Alauddin Jahin <alauddinjahin24@gmail.com>
2022-06-13 16:37:32 +0600

    updated & changed file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "flag-icon-css/css/flag-icons.min.css"
+import { useTranslation } from 'react-i18next';
+import i18next from "i18next";
+import { DropdownButton, Dropdown } from 'react-bootstrap';
+import { BsGlobe } from "react-icons/bs"
+import cookies from 'js-cookie';
+import { useEffect } from 'react';
+
+
+
+const languages = [
+  {
+    code: 'en',
+    name:'English',
+    country_code:'gb',
+  },
+  {
+    code: 'bn',
+    name:'বাংলাদেশ',
+    country_code:'bd',
+  },
+  {
+    code: 'ar',
+    name: 'العربية',
+    country_code:'sa',
+    dir: 'rtl',
+  },
+  {
+    code: 'fr',
+    name:'Francais',
+    country_code:'fr',
+  },
+];
 
 function App() {
+
+  const currentLanguageCode = cookies.get('i18next') || 'en'
+  const currentLanguage = languages.find((l) => l.code === currentLanguageCode)
+
+  const { t } = useTranslation();
+  const releaseDate = new Date("2021-03-07");
+  const timeDiff = new Date() - releaseDate;
+  const number_of_days = Math.floor(timeDiff / (1000 * 60 * 60 * 24))
+
+  useEffect(() => {
+    document.body.dir = currentLanguage.dir || 'ltr'
+    document.title    = t('app_title')
+  }, [currentLanguage, t])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{t('welcome_message')}</h2>
+      <p>{t('days_since_release', { number_of_days })}</p>
+
+      <DropdownButton
+        variant="link"
+        title={<BsGlobe/>}
+        id="input-group-dropdown-1"
+      >
+        <Dropdown.Item>
+        <span>{t('language')}</span>
+        </Dropdown.Item>
+        {
+          languages.map(({code, name, country_code})=>(
+            <Dropdown.Item key={country_code} 
+            onClick={() => i18next.changeLanguage(code)}
+              disabled={code === currentLanguageCode}
+            >
+              <span className={`flag-icon flag-icon-${country_code} mx-2`}></span>
+              {name}</Dropdown.Item>
+          ))
+        }
+      </DropdownButton>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
-import React from 'react';
+import React, { Suspense }  from 'react';
 import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import i18n, { use } from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+
+i18n
+.use(initReactI18next)
+.use(LanguageDetector)
+.use(HttpApi)
+.init({
+  supportedLngs:['en','bn','fr','ar'],
+  // resources: {
+  //   en: {
+  //     translation: {
+  //       "Welcome to React": "Welcome to React and react-i18next"
+  //     }
+  //   },
+  //   bn: {
+  //     translation: {
+  //       "Welcome to React": "রিয়েক্ট প্রোজেক্ট এ আপনাকে স্বাগতম"
+  //     }
+  //   }
+  // },
+  // lng: document.querySelector("html").lang,
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false
+  },
+  detection: {
+    order: ['path','cookie', 'htmlTag', 'localStorage', 'sessionStorage', 'navigator', 'querystring', 'subdomain'],
+    caches: ['localStorage', 'cookie'],
+  },
+  backend: {
+    loadPath: '/assets/locales/{{lng}}/translation.json',
+  },
+  // react:{
+  //   useSuspense: false
+  // }
+});
+
+const loadingMarkup = (
+  <div className="py-4 text-center">
+    <h3>Loading..</h3>
+  </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <Suspense fallback={loadingMarkup}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Suspense>
+  );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
